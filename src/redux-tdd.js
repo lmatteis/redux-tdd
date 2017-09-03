a286eca4b419d7de23e949b6e435b683f7bcf8c6
lmatteis <lmatteis@gmail.com>
2017-09-03 15:46:38 +0200

    Move files and prepare for NPM

---
+++ src/redux-tdd.js
@@ -0,0 +1,53 @@
+class ReduxTdd {
+  constructor(s, render) {
+    this.state = { ...s };
+    this.render = render;
+    this.wrapper = render(this.state);
+    this.currentAction = null;
+  }
+
+  view() {
+    // this.wrapper = this.render(this.state);
+    this.wrapper = this.wrapper.setProps(this.state)
+    return this;
+  }
+
+  reducer(reducer) {
+    const newState = reducer(this.state, this.currentAction)
+    this.state = newState;
+    return this;
+  }
+
+  action(mockActionFn) {
+    expect(mockActionFn).toHaveBeenCalled();
+    this.currentAction = mockActionFn();
+    return this;
+  }
+
+  simulate(fn) {
+    const result = fn(this.wrapper)
+    return this;
+  }
+
+  toMatchAction(obj) {
+    expect(this.currentAction).toMatchObject(obj)
+    return this;
+  }
+
+  toMatchState(obj) {
+    expect(this.state).toMatchObject(obj)
+    return this;
+  }
+
+  contains(arg) {
+    expect(this.wrapper.containsMatchingElement(arg)).toBeTruthy();
+    return this;
+  }
+
+  debug(cb) {
+    cb(this);
+    return this;
+  }
+}
+
+export default ReduxTdd;
--- test/test.jsx
+++ test/test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import ReduxTdd from '../src/redux-tdd';
 
 function Counter({ onIncrement, onReset, count }) {
   return (
@@ -11,58 +12,6 @@ function Counter({ onIncrement, onReset, count }) {
       }
     </div>
   );
-}
-
-class ReduxTdd {
-  constructor(s, render) {
-    this.state = { ...s };
-    this.render = render;
-    this.wrapper = render(this.state);
-    this.currentAction = null;
-  }
-
-  view() {
-    // this.wrapper = this.render(this.state);
-    this.wrapper = this.wrapper.setProps(this.state)
-    return this;
-  }
-
-  reducer(reducer) {
-    const newState = reducer(this.state, this.currentAction)
-    this.state = newState;
-    return this;
-  }
-
-  action(mockActionFn) {
-    expect(mockActionFn).toHaveBeenCalled();
-    this.currentAction = mockActionFn();
-    return this;
-  }
-
-  simulate(fn) {
-    const result = fn(this.wrapper)
-    return this;
-  }
-
-  toMatchAction(obj) {
-    expect(this.currentAction).toMatchObject(obj)
-    return this;
-  }
-
-  toMatchState(obj) {
-    expect(this.state).toMatchObject(obj)
-    return this;
-  }
-
-  contains(arg) {
-    expect(this.wrapper.containsMatchingElement(arg)).toBeTruthy();
-    return this;
-  }
-
-  debug(cb) {
-    cb(this);
-    return this;
-  }
 }
 
 function incrementAction() {
@@ -87,7 +36,6 @@ function reducer(state = { count: 0 }, action) {
       return state
   }
 }
-
 
 describe('<Counter />', () => {
   it('should test increment', () => {
