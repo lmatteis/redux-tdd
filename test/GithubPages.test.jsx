f2af4704143199a63553f3d72570a3bc6f7564a4
Luca Matteis <lmatteis@gmail.com>
2017-10-15 20:59:06 -0700

    New github page app

--- test/GithubPages.test.jsx
+++ test/GithubPages.test.jsx
@@ -12,11 +12,21 @@ function items(state = [], action) {
       return state
   }
 }
-function error(state, action) {
-  return {};
+function error(state = null, action) {
+  switch (action.type) {
+    case 'REFRESH_FAIL':
+      return action.payload.error
+    default:
+      return state;
+  }
 }
-function pages(state, action) {
-  return {}
+function pages(state = { perPage: 2, selectedPage: 0 }, action) {
+  switch (action.type) {
+    case 'SELECT_PAGE':
+      return { ...state, selectedPage: action.payload }
+    default:
+      return state
+  }
 }
 function loading(state = false, action) {
   switch (action.type) {
@@ -57,8 +67,8 @@ function refreshDoneAction(payload) {
 function refreshFailAction(payload) {
   return { type: 'REFRESH_FAIL', payload, error: true };
 }
-function pageClickAction() {
-  return {}
+function selectPageAction(pageIdx) {
+  return { type: 'SELECT_PAGE', payload: pageIdx }
 }
 
 function handleRefreshEpic(action$, store, { getJSON }) {
@@ -71,13 +81,24 @@ function handleRefreshEpic(action$, store, { getJSON }) {
 }
 
 function getPages(state) {
-  return state.items.map((item, idx) => idx)
+  const numberOfPages = Math.floor(state.items.length / state.pages.perPage) + 1;
+  var ret = []
+  for (var i=0; i<numberOfPages; i++) {
+    ret.push(i)
+  }
+  return ret;
+}
+function getItems(state) {
+  return state.items.slice(
+    state.pages.selectedPage * state.pages.perPage,
+    (state.pages.selectedPage * state.pages.perPage) + state.pages.perPage
+  )
 }
 
 describe('GithubPages', () => {
-  ReduxTdd({ items, error, loading }, state => [
+  ReduxTdd({ items, error, loading, pages }, state => [
     <List
-      items={state.items} />,
+      items={getItems(state)} />,
     <Loading
       loading={state.loading} />,
     <RefreshButton
@@ -86,39 +107,61 @@ describe('GithubPages', () => {
       message={state.error} />,
     <Pages
       pages={getPages(state)}
-      onPageClick={pageClickAction} />
+      onPageClick={selectPageAction} />
   ])
   .it('should not show any items')
-  .contains(<Item />, false)
+    .contains(<Item />, false)
 
   .it('should click on refresh button')
-  .switch(2) // RefreshButton
-  .action(props => props.onClick())
+    .switch(2) // RefreshButton
+    .action(props => props.onClick())
 
   .it('should show loading')
-  .switch(1) // Loading
-  .toMatchProps({ loading: true })
+    .switch(1) // Loading
+    .toMatchProps({ loading: true })
 
   .it('should trigger a successful HTTP response')
-  .epic(handleRefreshEpic, { getJSON: () =>
-    Observable.of([
-      { name: 'redux-tdd' }, { name: 'redux-cycles' }
-    ])
-  })
+    .epic(handleRefreshEpic, { getJSON: () =>
+      Observable.of([
+        { name: 'redux-tdd' }, { name: 'redux-cycles' }, { name: 'foo bar'}
+      ])
+    })
 
-  .it('should show projects')
-  .switch(0)
-  .toMatchProps({ items: [
-    { name: 'redux-tdd' }, { name: 'redux-cycles' }
-  ]})
+  .it('should show items based on the number of page selected')
+    .switch(0)
+    .toMatchProps({ items: [
+      { name: 'redux-tdd' }, { name: 'redux-cycles' }
+    ]})
 
   .it('should hide loading')
-  .switch(1)
-  .toMatchProps({ loading: false })
+    .switch(1)
+    .toMatchProps({ loading: false })
 
   .it('should render correct amount of pages')
-  .switch(4)
-  .toMatchProps({ pages: [0, 1] })
+    .switch(4)
+    .toMatchProps({ pages: [0, 1] })
+
+  .it('should select second page (index 1)')
+    .action(props => props.onPageClick(1))
+
+  .it('should show items based on second page')
+    .switch(0)
+    .toMatchProps({ items: [
+      { name: 'foo bar' }
+    ]})
+
+  .it('should trigger an error response')
+    .switch(2) // RefreshButton
+    .action(props => props.onClick())
+    .epic(handleRefreshEpic, { getJSON: () =>
+      Observable.throw({ error: 'Some error' })
+    })
+
+  .it('should test that Error component got right error message')
+    .switch(3)
+    .toMatchProps({
+      message: 'Some error'
+    })
 
 
 })
